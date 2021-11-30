b40dd04e43a1665524a38d621187cbc87d8ff6bd
Thyago Casablancas <thyago_casablancas@hotmail.com>
2021-11-30 18:22:13 -0300

    Realocating and Renaming
    Realocating and renaming files and folders wich will be used in the show up.

---
+++ DigioTest/ViewControllers/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewControllerswift
+//  DigioTest
+//
+//  Created by Thiago Augusto da Costa Silva on 30/11/21.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .lightGray
+    }
+}
