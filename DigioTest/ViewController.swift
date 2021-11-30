c9984498504c7b0266232935c55ee900dbef501a
Thyago Casablancas <thyago_casablancas@hotmail.com>
2021-11-30 18:02:16 -0300

    Initial Commit

---
+++ DigioTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DigioTest
+//
+//  Created by Thiago Augusto da Costa Silva on 30/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
